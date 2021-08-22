d0024ee8b4aeb2502e0ca99d71d1dbbb113b628e
Tuan_Anh_Dau <76159085+andrey551@users.noreply.github.com>
2021-08-22 21:08:27 +0300

    HW5

--- hello.py
+++ hello.py
@@ -1,52 +1,102 @@
-from flask import Flask
-from flask import render_template, request, make_response
-from werkzeug.utils import redirect
-
+from flask import Flask, request, render_template, make_response, redirect, flash
+import app_config
+from model.user import User
+from functools import wraps
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = app_config.SECRET_KEY
 
-auth_token = '3h1e4l1l5o9p2r6i5v3e5t'
+users = {name[:-5]:User.from_file(name) for name in os.listdir(app_config.USER_DB_DIR)}
 
-def set_token(user, password):
-    return '3h1e4l1l5o9p2r6i5v3e5t'
+def login_required(func):
+    @wraps(func)
+    def login_func(*arg, **kwargs):
+        try:
+            if (users[request.cookies.get('username')].authorize(request.cookies.get('token'))):
+                return func(*arg, **kwargs)
+        except:
+            pass
+        flash("Login required!!!")
+        return redirect('/login')
+    
+    return login_func
 
-def auth(request):
-    global auth_token
-    token = request.cookies.get('login_info')
-    return (token == auth_token)
+def no_login(func):
+    @wraps(func)
+    def no_login_func(*arg, **kwargs):
+        try:
+            if (users[request.cookies.get('username')].authorize(request.cookies.get('token'))):
+                flash("You're already in!")
+                return redirect('/')
+        except:
+            pass
+        return func(*arg, **kwargs)
+    
+    return no_login_func
 
-@app.route("/")
-def apps():
-        return render_template("login.html")
+@app.route('/')
+def home():
+    return redirect('/index')
 
-@app.route("/login", methods = ["GET", "POST"])
+@app.route('/index')
+@login_required
+def index():
+    return render_template('main.html')
+
+@app.route('/login', methods=['POST', 'GET'])
+@no_login
 def login():
     if request.method == 'GET':
-        return redirect('/')
-    else :
-        user = request.form.get('userName')
-        password = request.form.get('password')
-        if (user == 'admin' and password == 'admin'):
-            token = set_token(user, password)
-            respond = make_response(redirect('/index'))
-            respond.set_cookie('login_info', token)
-            return respond
+        return render_template('login.html')
+    
+    username, password = request.form.get('username'), request.form.get('password')
+    if username in users.keys():
+        if users[username].authenticate(password):
+            token = users[username].init_session()
+            resp = make_response(redirect('/index'))
+            resp.set_cookie('username', username)
+            resp.set_cookie('token', token)
+            return resp
         else:
-            return redirect('/')
-
-@app.route('/index', methods = ["POST", "GET"])
-def index():
-    if auth(request):
-        return render_template("main.html")
+            flash("Username or password is not correct!!!")
     else:
-        return redirect('/')
+        flash("User does not exist")
+    
+    return render_template('login.html')
 
+@app.route('/logout', methods=['GET','POST'])
+@login_required
+def logout():
+    username = request.cookies.get('username')
+    users[username].terminate_session()
+    resp = make_response(redirect('/login'))
+    resp.delete_cookie('username')
+    resp.delete_cookie('token')
+    flash("You've logged out!!!")
+    return resp
 
-@app.route("/log_out", methods=["POST","GET"])
-def log_out():
-    respond = make_response(redirect('/index'))
-    respond.set_cookie('login_info', '', expires=0)
-    return respond
+@app.route('/register', methods=['POST', 'GET'])
+@no_login
+def register():
+    if request.method == "GET":
+        return render_template('register.html')
+    
+    username, password, password_confirm = request.form.get('username'), request.form.get('password'), request.form.get('password_confirm')
+    if username not in users.keys():
+        if password == password_confirm:
+            users[username] = User.new(username, password)
+            token = users[username].init_session()
+            resp = make_response(redirect('/index'))
+            resp.set_cookie('username', username)
+            resp.set_cookie('token', token)
+            return resp
+        else:
+            flash("Passwords don't match!!!")
+    else:
+        flash("User already exists!!!")
+    
+    return render_template('register.html')
 
-if __name__ == "__main__":
-    app.run(host="localhost", port='5000')
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000, debug=True)
